4c690850fe52bc3a4725173b928ea389257d39b5
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-05 03:26:07 +0300

    refactor our code

--- lib/views/chat_view.dart
+++ lib/views/chat_view.dart
@@ -44,17 +44,17 @@ class _ChatViewState extends State<ChatView> {
           icon: const Icon(Icons.logout),
         ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: messages.orderBy(kCreatedAt, descending: true).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List<Message> messagesList = [];
-            for (int i = 0; i < snapshot.data!.docs.length; i++) {
-              messagesList.add(Message.fromJson(snapshot.data!.docs[i]));
-            }
-            return Column(
-              children: [
-                Expanded(
+      body: Column(
+        children: [
+          StreamBuilder<QuerySnapshot>(
+            stream: messages.orderBy(kCreatedAt, descending: true).snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Message> messagesList = [];
+                for (int i = 0; i < snapshot.data!.docs.length; i++) {
+                  messagesList.add(Message.fromJson(snapshot.data!.docs[i]));
+                }
+                return Expanded(
                   child: ListView.builder(
                       reverse: true,
                       controller: _controller,
@@ -66,48 +66,48 @@ class _ChatViewState extends State<ChatView> {
                               )
                             : ChatBubbleForFriend(message: messagesList[index]);
                       }),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: controller,
+              onSubmitted: (data) {
+                messages.add({
+                  kMessage: data,
+                  kCreatedAt: DateTime.now(),
+                  kId: email,
+                });
+                controller.clear();
+                _controller.animateTo(
+                  0,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeIn,
+                );
+              },
+              decoration: InputDecoration(
+                hintText: 'Send Message',
+                suffixIcon: const Icon(
+                  Icons.send,
+                  color: kPrimaryColor,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: controller,
-                    onSubmitted: (data) {
-                      messages.add({
-                        kMessage: data,
-                        kCreatedAt: DateTime.now(),
-                        kId: email,
-                      });
-                      controller.clear();
-                      _controller.animateTo(
-                        0,
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeIn,
-                      );
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Send Message',
-                      suffixIcon: const Icon(
-                        Icons.send,
-                        color: kPrimaryColor,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: const BorderSide(color: kPrimaryColor),
-                      ),
-                    ),
-                  ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
                 ),
-              ],
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(color: kPrimaryColor),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
